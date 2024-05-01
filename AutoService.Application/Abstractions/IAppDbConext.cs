f10f580986e27d1a484924d5a6318d207edaed3f
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-05-01 17:30:46 +0500

    add new add chekced  confugurate

--- AutoService.Application/Abstractions/IAppDbConext.cs
+++ AutoService.Application/Abstractions/IAppDbConext.cs
@@ -1,6 +1,7 @@
-﻿using AutoService.Domain.Entities.Models.CarModels;
+﻿using AutoService.Domain.Entities.Models.AutoServiceModels;
+using AutoService.Domain.Entities.Models.CarModels;
 using AutoService.Domain.Entities.Models.UserModels;
-using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
 
 namespace AutoService.Application.Abstractions
 {
@@ -8,5 +9,8 @@ namespace AutoService.Application.Abstractions
     {
         public DbSet<CarModel> carModels { get; set; }
         public DbSet<UserQuestions> UserQuestions { get; set; }
+        public DbSet<AutoServiceModel> autoServices { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
