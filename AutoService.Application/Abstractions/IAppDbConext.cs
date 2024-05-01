eb67651e297014e33d5d3134508c19a37a5d540e
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-05-01 16:34:11 +0500

    add app db context

--- AutoService.Application/Abstractions/IAppDbConext.cs
+++ AutoService.Application/Abstractions/IAppDbConext.cs
@@ -1,7 +1,12 @@
-﻿namespace AutoService.Application.Abstractions
+﻿using AutoService.Domain.Entities.Models.CarModels;
+using AutoService.Domain.Entities.Models.UserModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoService.Application.Abstractions
 {
     public class IAppDbConext
     {
-        
+        public DbSet<CarModel> carModels { get; set; }
+        public DbSet<UserQuestions> UserQuestions { get; set; }
     }
 }
