9c2f9f8d9fde0addebc8c8f37c3bb66d15e21225
Ozodbek <145344998+OlimjonovOzodbek@users.noreply.github.com>
2024-05-01 21:39:10 +0500

    AutoService create

---
+++ AutoService.Application/AutoService/AutoServiceCases/Commands/CreateAutoServiceCommand.cs
@@ -0,0 +1,20 @@
+﻿using AutoService.Domain.Entities.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService.Application.AutoService.AutoServiceCases.Commands
+{
+    public class CreateAutoServiceCommand:IRequest<ResponceModel>
+    {
+        public string ServiceName { get; set; }
+        public string City { get; set; }
+        public string WebSitePath { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerNumber { get; set; }
+        public string OwnerEmail { get; set; }
+    }
+}
